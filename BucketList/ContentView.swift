0e68838df04b627c0525dc841e41205693ba15b9
Issac Penn <pbbsignup@outlook.com>
2019-12-06 12:19:38 +0800

    Initial Commit

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by PBB on 2019/12/6.
+//  Copyright © 2019 PBB. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
