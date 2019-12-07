457118469248e4f2ed445508feee1c16ed571206
Issac Penn <pbbsignup@outlook.com>
2019-12-07 19:07:48 +0800

    Fixed a bug where data from wikipedia cannot be decoded

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -29,7 +29,7 @@ struct EditView: View {
                 
                 Section(header: Text("Nearby...")) {
                     if loadingState == .loaded {
-                        List(pages, id: \.pageId) { page in
+                        List(pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ")
@@ -53,6 +53,7 @@ struct EditView: View {
     
     func fetchNearbyPlaces() {
         let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        print("URL: \(urlString)")
         
         guard let url = URL(string: urlString) else {
             print("Bad URL: \(urlString)")
